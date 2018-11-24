bf1fc63481698990dbd43cea92a401690bc9ebeb
dannertech <danner@danner.tech>
2018-11-24 15:47:29 -0500

    completed first part of constraints tutorial from Ray Wenderlich

---
+++ Buttons/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Buttons
+//
+//  Created by Diamonique Danner on 11/24/18.
+//  Copyright © 2018 Danner Opp., LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.title(for: .normal) == "X" {
+            sender.setTitle("A very long title for this button", for: .normal)
+        } else {
+            sender.setTitle("X", for: .normal)
+        }
+    }
+
+}
+
